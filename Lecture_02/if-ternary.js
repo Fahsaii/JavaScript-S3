20b6fead93adc2a27e0c51d039e412a63d19e4e6
fahsaii <fahsai.tpd@gmail.com>
2024-12-25 12:08:32 +0700

    test

--- Lecture_02/if-ternary.js
+++
@@ -1,5 +0,0 @@
-const status = 200;
-
-const message = (status === 200) ? 'KO!' : 'Error';
-
-console.log(message);
--- Lecture_02/undefined.js
+++
@@ -1,9 +0,0 @@
-function test(t){
-    if(t === undefined) {
-        return "Undefind value!";
-    }
-    return t;
-}
-
-let x = 5;
-console.log(test(x));
