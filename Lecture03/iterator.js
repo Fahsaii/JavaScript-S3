5ddc78d84c52a96a6acbbb938b4e46dbce988d73
Fahsaii <97835163+Fahsaii@users.noreply.github.com>
2024-12-25 11:54:05 +0700

    Delete Lecture03 directory

--- Lecture03/iterator.js
+++
@@ -1,4 +0,0 @@
-for (let char of "test") {
-    // triggers 4 times: once for each charater
-    console.log( char ); // t, then e, then s, then t
-}
--- Lecture03/maparray.js
+++
@@ -1,7 +0,0 @@
-const finalPartipants = ["Taylor", "Donald", "Don", "Natasha", "Bobby"];
-
-const announcements = finalPartipants.map((member) => {
-    return member + " joined the contest.";
-});
-
-console.log(announcements);
